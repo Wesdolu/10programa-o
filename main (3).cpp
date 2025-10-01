9345a540eace941908566425ccb9909a70745b9e
Wesdolu <wesley04052011@gmail.com>
2025-10-01 12:17:41 +0100

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main() {
+    int kmi,kmf,GasolinaGasta,resultado;
+    std::cout<<"Quantos litros de gasolina gastaste";
+    std::cin>>Gasolina
+    std::cout<<"Kilómetros iniciais";
+    std::cin>>Kmi
+    std::cout<<"Kilómetros finais";
+    std::cin>>kmf
+    Resultado=(100*Gasolina)/Kmf-Kmi
+
+    return 0;
+}
