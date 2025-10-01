262b8118068b88afca1bfbae7e5556371c0a8ab3
Wesdolu <wesley04052011@gmail.com>
2025-10-01 12:58:04 +0100

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int numero
+    std::cout<<"escreve um número";
+    std::cin>>número
+    if(número>0)
+    std::cout<<"Positivo";
+    else
+    std::cout<<"Negativo"
+    return 0;
+}
