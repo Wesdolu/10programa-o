a1c8b0a061fd356808fb3e0f69b5a83c7a11c14c
Wesdolu <wesley04052011@gmail.com>
2025-11-06 16:40:27 +0000

    Add files via upload

---
+++ qualcarroquer.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	string carros[3] = { "Volvo", "BMW", "Ford" };
+	int n;
+		char sair;
+		sair = 'n';
+
+    do
+	{
+			cout << " Escolha um carro ( 0 a 2):\n ";
+			cin >> n;
+		
+				if (n < 0 || n > 2)
+				{
+		cout << " Esse n é inválido. Tente novamente.\n";
+
+		}
+	 else
+	    {
+		 cout << " O carro escolhido foi: " << carros[n] << "\n";
+		}
+		cout << "Deseja sair? (s/n):\n ";
+		cin >> sair;
+	} while (sair =='n');
+	return 0;
+}
