ea829da0d48c318577b41d6620bbc34d85f1f035
Wesdolu <wesley04052011@gmail.com>
2025-11-06 16:39:16 +0000

    Add files via upload

---
+++ calculoschatos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int num[10]; {} "-4", "84", "-76", "23", "54", "-12", "9", "-5", "-3", "-1";
+	do {
+		int soma = 0;
+		cout << "Quantos num negativos  ";
+		cout << "A soma dos num negativos  ";
+	} while (num != 0);
+	
+	return 0;
+}
