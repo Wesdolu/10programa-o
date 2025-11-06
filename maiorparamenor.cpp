5cb3483045be611001dacc8f3c2c78cc6fbd0e82
Wesdolu <wesley04052011@gmail.com>
2025-11-06 16:41:12 +0000

    Add files via upload

---
+++ maiorparamenor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0));
+	int aux1;
+	int bolha[10] = { 15,2,53,4,1,17,5,23,45,19 };
+	do
+	{
+		aux1 = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			if (bolha[i] > bolha[i + 1])
+			{
+				aux1 = bolha[i];
+				bolha[i] = bolha[i + 1];
+				bolha[i + 1] = aux1;
+			
+			}
+			
+		}
+	} while (aux1!=0);
+	for (int i = 0; i < 10; i++)
+		cout << bolha[i] << " ";
+	return 0;
+} 
